2f16cb38892391bd7a910edd16b44868de0bdd28
vitor <rike2008@gmail.com>
2020-02-17 03:12:35 -0300

    Criando os atributos da entidade Book

--- src/main/java/com/example/demo/models/Book.java
+++ src/main/java/com/example/demo/models/Book.java
@@ -1,4 +1,54 @@
 package com.example.demo.models;
 
-public class Book{
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.*;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy= AUTO)
+    private Long id;
+
+    @Column(name="title", nullable=false)
+    private String title;
+
+    @Column(name="isbn", nullable=false)
+    private String isbn;
+
+    @Column(name="autor", nullable=false)
+    private String autor;
+
+    public Long getId (){
+        return id;
+    }
+
+    public void setId (Long id){
+        this.id = id;
+    }
+
+    public String getTitle (){
+        return title;
+    }
+
+    public void setTitle (String title){
+        this.title = title;
+    }
+
+    public String getIsbn (){
+        return isbn;
+    }
+
+    public void setIsbn (String isbn){
+        this.isbn = isbn;
+    }
+
+    public String getAutor (){
+        return autor;
+    }
+
+    public void setAutor (String autor){
+        this.autor = autor;
+    }
 }
