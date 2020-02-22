de83309097698bc1fa8ada59993e9c64a427d312
vitor <rike2008@gmail.com>
2020-02-22 14:33:31 -0300

    CRUD author

--- src/main/java/com/example/demo/controllers/AuthorController.java
+++ src/main/java/com/example/demo/controllers/AuthorController.java
@@ -2,10 +2,13 @@ package com.example.demo.controllers;
 
 import com.example.demo.models.Author;
 import com.example.demo.repositories.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
@@ -14,19 +17,38 @@ import javax.validation.Valid;
 @RequestMapping("/")
 public class AuthorController {
 
-    AuthorRepository authorRepository;
+    private final AuthorRepository authorRepository;
 
-    @GetMapping("/")
-    public String listAutors (@Valid Author author, BindingResult bindingResult, Model model) {
-        model.addAttribute("authors", authorRepository.findAll());
-        return "author/list";
+    @Autowired
+    public AuthorController (AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
     }
 
-    @GetMapping("/add-author")
-    public String login (@Valid Author author, BindingResult bindingResult, Model model) {
+    @GetMapping("login")
+    public String showSignUpForm (Author author) {
         return "author/add";
     }
 
+    @GetMapping("list-authors")
+    public String showUpdateForm (Model model) {
+        model.addAttribute("author", authorRepository.findAll());
+        return "author/list";
+    }
 
+    @PostMapping("add")
+    public String addAuthor (@Valid Author author, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "author/add";
+        }
+        authorRepository.save(author);
+        return "redirect:author/list";
+    }
+
+    @GetMapping("edit/{id}")
+    public String showUpdateForm (@PathVariable("id") long id, Model model) {
+        Author author = authorRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Código de verificação número " + id + " inválido!"));
+        model.addAttribute("author", author);
+        return "author/update";
+    }
 
 }
--- src/main/java/com/example/demo/repositories/AuthorRepository.java
+++ src/main/java/com/example/demo/repositories/AuthorRepository.java
@@ -1,8 +1,9 @@
 package com.example.demo.repositories;
 
+import com.example.demo.models.Author;
 import com.example.demo.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.lang.Long;
 
-public interface AuthorRepository extends JpaRepository<Book, Long>{
+public interface AuthorRepository extends JpaRepository<Author, Long>{
 }
