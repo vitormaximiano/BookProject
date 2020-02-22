53fb5f22ea2bd92e9638ef66c1a62ce41c8b60f3
vitor <rike2008@gmail.com>
2020-02-22 11:28:32 -0300

    Author class create

--- src/main/java/com/example/demo/repositories/AutorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.models.Autor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface AutorRepository extends JpaRepository<Autor, Long>{
-}
