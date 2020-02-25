1fa586589b3ed268acb2a33e4dacf00ce8874b95
vitor <rike2008@gmail.com>
2020-02-25 17:50:42 -0300

    Correction classes names

--- src/main/java/com/example/demo/repositories/BookRepository.java
+++ src/main/java/com/example/demo/repositories/BookRepository.java
@@ -1,6 +1,8 @@
 package com.example.demo.repositories;
 
+import com.example.demo.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Books, Long> {
+
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
